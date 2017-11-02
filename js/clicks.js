1194cb318af9ce9bc24bc56bec8affd93092b43b
szelazny <scott_z_1@yahoo.com>
2017-11-02 14:22:07 -0400

    Update clicks.js

--- js/clicks.js
+++ js/clicks.js
@@ -10,7 +10,6 @@ var Model = {
       {"name": "Rudy",       "numClicks": 0},
       {"name": "Rudy2",      "numClicks": 0},
       {"name": "Rudy3",      "numClicks": 0},
-      {"name": "Sabrina",    "numClicks": 0},
       {"name": "Sabrina2",   "numClicks": 0}
     ],
 
